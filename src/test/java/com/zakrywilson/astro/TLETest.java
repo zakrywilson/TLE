fbc847f3ad0923129e97493812423770c83e7cc3
zakrywilson <zakrywilson@gmail.com>
2016-11-21 10:43:46 -0700

    Created test class for TLE

---
+++ src/test/java/com/zakrywilson/astro/TLETest.java
@@ -0,0 +1,28 @@
+package com.zakrywilson.astro;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TLETest {
+
+    @Test
+    public void getTitleLine() throws Exception {
+        Assert.fail();
+    }
+
+    @Test
+    public void getLine1() throws Exception {
+        Assert.fail();
+    }
+
+    @Test
+    public void getLine2() throws Exception {
+        Assert.fail();
+    }
+
+    @Test
+    public void toStringOverride() throws Exception {
+        Assert.fail();
+    }
+
+}
